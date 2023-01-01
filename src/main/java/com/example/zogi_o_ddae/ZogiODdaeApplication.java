4699c7dd8eceae37806a920cc7f79fc574150d88
dksekfbs72 <dksekfbs72@naver.com>
2023-01-01 19:30:28 +0900

    project_init

---
+++ src/main/java/com/example/zogi_o_ddae/ZogiODdaeApplication.java
@@ -0,0 +1,13 @@
+package com.example.zogi_o_ddae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZogiODdaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZogiODdaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zogi_o_ddae/ZogiODdaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zogi_o_ddae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZogiODdaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
