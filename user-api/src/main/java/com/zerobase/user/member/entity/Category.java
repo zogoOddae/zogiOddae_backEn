3fe7777d01a88510bc4970706b14a9c9f067b684
hyejin0662 <hyejin0776@gmail.com>
2023-01-18 01:22:11 +0900

    feat)[HYEJIN] Cartegory_Update

--- user-api/src/main/java/com/zerobase/user/member/entity/Category.java
+++ user-api/src/main/java/com/zerobase/user/member/entity/Category.java
@@ -1,5 +1,34 @@
 package com.zerobase.user.member.entity;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.Criteria;
+
+@Entity
+@Getter @Setter
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "category_id", nullable = false)
+    private Long category_id;
+
+    private Long id;
+
+    private String name;
+    @Getter
+    @OneToMany(mappedBy = "category")
+    private List<Notice> notices = new ArrayList<>();
+
 
 }
---
+++ user-api/src/main/java/com/zerobase/user/member/repository/CategoryRepository.java
@@ -0,0 +1,25 @@
+package com.zerobase.user.member.repository;
+
+import com.zerobase.user.member.entity.Category;
+import com.zerobase.user.member.entity.Member;
+import com.zerobase.user.member.entity.Notice;
+import java.awt.print.Pageable;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    public Category findByName(String categoryName);
+
+    public Long notice(Long memberId, Long categoryId, String subject, String description);
+    public Long updateCategory(Long noticeId, Long categoryId, String subject, String description);
+    public Notice findOne(Long categoryId);
+    public List<Category> findAll();
+    public List<Category> findByMember(Member member);
+    public List<Category> findByNotice(Notice notice);
+    public void deleteCategory(Long categoryId);
+
+
+}
