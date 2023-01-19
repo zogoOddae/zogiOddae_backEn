83e5bc5ffa37a24b3f5be301dfe871c11d53e1e6
hyejin0662 <hyejin0776@gmail.com>
2023-01-20 01:09:24 +0900

    feat)[HYEJIN] Category_and_Paging

--- user-api/src/main/java/com/zerobase/user/member/service/CategoryService.java
+++ user-api/src/main/java/com/zerobase/user/member/service/CategoryService.java
@@ -1,5 +1,11 @@
 package com.zerobase.user.member.service;
 
-public class CategoryService {
+import com.zerobase.user.member.entity.Member;
+import com.zerobase.user.member.entity.Notice;
+import java.util.List;
+
+public interface CategoryService  {
+    public List<Notice> findAll();
+    public List<Notice> findByMember(Member member);
 
 }
