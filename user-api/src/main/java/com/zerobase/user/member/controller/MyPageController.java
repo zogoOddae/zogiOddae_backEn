749e8e0d8da370a83ca4a75c2080537915e70d3f
hyejin0662 <hyejin0776@gmail.com>
2023-01-20 01:10:47 +0900

    feat)[HYEJIN] MyPageController_Update

--- user-api/src/main/java/com/zerobase/user/member/controller/MyPageController.java
+++ user-api/src/main/java/com/zerobase/user/member/controller/MyPageController.java
@@ -1,23 +1,42 @@
 package com.zerobase.user.member.controller;
 
-
+import com.zerobase.user.member.dto.UpdatePassword;
+import com.zerobase.user.member.dto.form.MemberForm;
+import com.zerobase.user.member.entity.Category;
 import com.zerobase.user.member.entity.Member;
-import com.zerobase.user.member.repository.MemberRepository;
+import com.zerobase.user.member.entity.Notice;
+import com.zerobase.user.member.service.CategoryService;
+import com.zerobase.user.member.service.CommentService;
+import com.zerobase.user.member.service.MemberServiceInter;
+import com.zerobase.user.member.service.NoticeService;
+import java.awt.print.Pageable;
+import java.util.List;
+import java.util.Optional;
+import org.hibernate.annotations.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 public class MyPageController {
 
-    private final MemberRepository memberRepository;
+    private final NoticeService noticeService;
+    private final MemberServiceInter memberService;
+    private final CategoryService categoryService;
+    private CommentService commentService;
 
-    public MyPageController(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
+    public MyPageController(NoticeService noticeService, MemberServiceInter memberService,
+            CategoryService categoryService) {
+        this.noticeService = noticeService;
+        this.memberService = memberService;
+        this.categoryService = categoryService;
     }
+
 
     @GetMapping("/mypage/me")
     public String myPageHome(Model model, @AuthenticationPrincipal Member currentMember) {
@@ -28,8 +47,6 @@ public class MyPageController {
 
         model.addAttribute("memberForm", memberForm);
         return "mypage/me";
-
-
     }
 
     @PostMapping("/mypage/me")
@@ -38,7 +55,7 @@ public class MyPageController {
             return "redirect:/mypage/me";
         }
 
-        memberRepository.updateInfo(currentMember.getUsername(), form.getName(), form.getEmail());
+        memberService.mypage(currentMember.getUsername(),currentMember.getEmail(), form.getName(), form.getEmail());
         currentMember.setUsername(form.getName());
         currentMember.setEmail(form.getEmail());
 
@@ -47,6 +64,67 @@ public class MyPageController {
 
 
 
+    @GetMapping("/mypage/comments")
+    public String myComments(Model model, @AuthenticationPrincipal Member currentMember,
+            @PageableDefault Pageable pageable) {
+        Member member = (Member) memberService.findByMembername(currentMember.getUsername())
+                .orElseThrow(()-> new UsernameNotFoundException(currentMember.getUsername()));
+        Optional<Object> comments = commentService.findListByMember(member, pageable);
+
+        List<Notice> categoryList = categoryService.findAll();
+
+        model.addAttribute("categoryList", categoryList);
+        model.addAttribute("comments", comments);
+        return "mypage/comments";
+    }
+    @PostMapping("/mypage/password")
+    public String passwordEdit(Model model,
+            UpdatePassword form,
+            BindingResult result,
+            @AuthenticationPrincipal Member currentMember) {
+        if (result.hasErrors()) {
+            return "redirect:/mypage/password";
+        }
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        if (!encoder.matches(form.getPassword(), currentMember.getPassword())) {
+            model.addAttribute("error", "현재 패스워드 불일치");
+            return "mypage/passwordError";
+        }
+
+        if(form.getNewPassword().equals(form.getPassword())){
+            model.addAttribute("error", "동일한 패스워드");
+            return "mypage/passwordError";
+        }
+
+        if (!form.getNewPassword().equals(form.getNewPasswordConfirm())) {
+            model.addAttribute("error", "새 패스워드 불일치");
+            return "mypage/passwordError";
+        }
+
+        String encodedNewPwd = encoder.encode(form.getNewPassword());
+        memberService.updatePassword(currentMember.getUsername(), encodedNewPwd);
+        currentMember.setPassword(encodedNewPwd);
+        return "redirect:/mypage/me";
+    }
+
+    /*
+    @GetMapping("/mypage/contents")
+    public String myContents(Model model, @AuthenticationPrincipal Member currentMember,
+            @PageableDefault Pageable pageable) {
+        Member member = memberService.findByUsername(currentMember.getUsername())
+                .orElseThrow(()-> new UsernameNotFoundException(currentMember.getUsername()));
+
+        Notice notices = NoticeService.getPostListByMember(member, pageable);
+
+        List<Notice> categoryList = categoryService.findAll();
+
+        model.addAttribute("categoryList", categoryList);
+        model.addAttribute("notices", notices);
+        return "mypage/contents";
+    }
+
+     */
 
 }
-
