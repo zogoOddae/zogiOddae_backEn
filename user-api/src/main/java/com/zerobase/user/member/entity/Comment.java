ae777f531cba66722c97e439a20932d508cfe5a7
hyejin0662 <hyejin0776@gmail.com>
2023-01-20 01:11:57 +0900

    feat)[HYEJIN] Comment_Update

---
+++ user-api/src/main/java/com/zerobase/user/member/entity/Comment.java
@@ -0,0 +1,10 @@
+package com.zerobase.user.member.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Comment {
+
+    private List<Comment> comments = new ArrayList<>();
+
+}
