6d29e0114103cddfd9107c8212789dad4f7b9420
dksekfbs72 <dksekfbs72@naver.com>
2023-01-26 04:05:32 +0900

    [다륜] 레포지토리 어노테이션 수정

--- leisure-api/src/main/java/com/zerobase/leisure/domain/repository/leisure/LeisureRepository.java
+++ leisure-api/src/main/java/com/zerobase/leisure/domain/repository/leisure/LeisureRepository.java
@@ -2,13 +2,13 @@ package com.zerobase.leisure.domain.repository.leisure;
 
 import com.zerobase.leisure.domain.entity.leisure.Leisure;
 import com.zerobase.leisure.domain.type.Category;
-import io.lettuce.core.dynamic.annotation.Param;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
--- leisure-api/src/main/java/com/zerobase/leisure/domain/repository/leisure/LeisureReservationDayRepository.java
+++ leisure-api/src/main/java/com/zerobase/leisure/domain/repository/leisure/LeisureReservationDayRepository.java
@@ -2,13 +2,12 @@ package com.zerobase.leisure.domain.repository.leisure;
 
 import com.zerobase.leisure.domain.entity.leisure.LeisureReservationDay;
 import com.zerobase.leisure.domain.model.LeisureIds;
-import io.lettuce.core.dynamic.annotation.Param;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
